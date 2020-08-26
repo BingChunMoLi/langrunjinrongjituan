b804e9dd691f06204f65d55864a04c920d8673c2
冰彦糖 <44092516+LeagueJhin@users.noreply.github.com>
2020-08-26 09:46:05 +0800

    轮播图完成，继续时区时间

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 冰彦糖
  * @Date: 2020-08-25 10:16:02
- * @LastEditTime: 2020-08-25 16:36:33
+ * @LastEditTime: 2020-08-26 09:45:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \浪润金融集团\js\index.js
@@ -16,8 +16,39 @@
             $(iterator).addClass('activer');
         }
     }
-    const navImg = $('#navImg');
     const navImgLeft = $('#navImgLeft');
     const navImgRight = $('#navImgRight');
-    $("#slider").easySlider({});
+    const navi = $('.navi');
+    let navia = 0;
+    navImgLeft.on("click",function () {
+        navia -= 860;
+        // navi.css('marginLeft',navia+'px');
+        navi.animate({"marginLeft":navia},1000)
+        if(navia == -2580){
+            navia = 0;
+        }
+    })
+    navImgRight.on("click",function () {
+        navia += 860;
+        if(navia == 860){
+            navia = -2580;
+        }
+        // navi.css('marginLeft',navia+'px');
+        navi.animate({"marginLeft":navia},1000);
+        if(navia == 2580){
+            navia = 0;
+        }
+    })
+    timer = setInterval(func, 1500);
+    function func() {
+        clearInterval(timer);
+        navia -= 860;
+        navi.animate({"marginLeft":navia},1000)
+        // navi.css('marginLeft',navia+'px');
+        if(navia == -2580){
+            navia = 0;
+        }
+    }
+    const dateTime = $(".dateTime");
+
 })()
